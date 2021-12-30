5e32ce22c918fbdec3cebd963fb0a9cd1908df42
Harshal623 <89270049+Harshal623@users.noreply.github.com>
2021-12-30 09:27:47 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -27,5 +27,60 @@ function block_update(new_image){
         canvas.add(block_object)
     });
 }
-player_update()
-block_update("wall.jpg")
+window.addEventListener("keydown", my_keydown);
+function my_keydown(e){
+    keypressed = e.keyCode
+    console.log(keypressed)
+    if (e.shiftKey == true && keypressed == '80'){
+        block_width += 10
+        block_height += 10
+        document.getElementById("c_width").innerHTML = block_width
+        document.getElementById("c_height").innerHTML = block_height
+    }
+    if (e.shiftKey == true && keypressed == '77'){
+        block_width -= 10
+        block_height -= 10
+        document.getElementById("c_width").innerHTML = block_width
+        document.getElementById("c_height").innerHTML = block_height
+    }
+    if (keypressed == '37'){
+        moveleft()
+    }
+    if (keypressed == '38'){
+        moveup()
+    }
+    if (keypressed == '39'){
+        moveright()
+    }
+    if (keypressed == '40'){
+        moveldown()
+    }
+    if (keypressed == '67'){
+        block_update("cloud.jpg")
+    }
+    if (keypressed == '68'){
+        block_update("dark_green.png")
+    }
+    if (keypressed == '71'){
+        block_update("ground.png")
+    }
+    if (keypressed == '76'){
+        block_update("light_green.png")
+    }
+    if (keypressed == '82'){
+        block_update("roof.jpg")
+    }
+    if (keypressed == '84'){
+        block_update("trunk.jpg")
+    }
+    if (keypressed == '85'){
+        block_update("unique.png")
+    }
+    if (keypressed == '87'){
+        block_update("wall.jpg")
+    }
+    if (keypressed == '89'){
+        block_update("yellow_wall.png")
+    }
+
+}
